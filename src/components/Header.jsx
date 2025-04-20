986661727f255b546366cd9796227206f1829d11
muhammadAli <muhammadAli@gamil.com>
2025-04-20 15:31:01 +0500

    6-oy1-dars

---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <nav className="header-container ">
+        <img src="../../img/logo.svg" alt="" />
+        <ul className="title-header">
+          <li className="elements">
+            <a className="elTitles" href="#">
+              Home
+            </a>
+          </li>
+          <li className="elements">
+            <a className="elTitles" href="#">
+              Skills
+            </a>
+          </li>
+          <li className="elements">
+            <a className="elTitles" href="#">
+              About Us
+            </a>
+          </li>
+          <li className="elements">
+            <a className="elTitles" href="#">
+              Pricing
+            </a>
+          </li>
+          <li className="elements">
+            <a className="elTitles" href="#">
+              Contacts
+            </a>
+          </li>
+        </ul>
+        <button className="headerBtn">
+          <a className="btnTitle" href="#">
+            Let’s Talk
+          </a>
+        </button>
+      </nav>
+    </header>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,22 @@
+import "./Main.css";
+
+export default function Main() {
+  return (
+    <main className="main">
+      <div className="main-container container">
+        <div className="section1">
+          <h1 className="title">Learn Any Foreign Language</h1>
+          <p className="text-content">
+            With our teachers who write a program for each student, you will be
+            able to make your first sketch after the first lesson.
+          </p>
+          <button className="getBtn">
+            <a className="getBtnTitile" href="#">Get started</a>
+          </button>
+        </div>
+        <img className="light-img" src="../../img/lighting.svg" alt="" />
+        <img className="main-img" src="../../img/hero-bg.svg" alt="" />
+      </div>
+    </main>
+  );
+}
